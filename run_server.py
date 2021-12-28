fd5c4ab36681d7a77b40800b34e7984d3637a242
Truman Daniels <34697650+trumandaniels@users.noreply.github.com>
2021-12-28 15:20:08 -0800

    Add files via upload

--- run_server.py
+++ run_server.py
@@ -1,16 +1,71 @@
-#data comes from https://www.kaggle.com/austinreese/craigslist-carstrucks-data
+#https://flask.palletsprojects.com/en/2.0.x/patterns/sqlalchemy/
 
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from sqlalchemy.sql import expression, functions
 from sqlalchemy import types
-from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, MetaData, Table
 
 app = Flask(__name__)
 api = Api(app)
-db_URL = 'sqlite:///auto-mpg.db'
-db = SQLAlchemy(app)
+
+engine = create_engine('sqlite:///auto-mpg.db')
+metadata = MetaData(bind=engine)
+
+def get_column_names(ENGINE=engine):
+    '''
+    Description:
+    Used to collect a list of all column names
+    with the database engine
+    
+    Parameters:
+    * Engine ~ A SQLAlchemy engine connected to sqlite database
+    * * * * Default: database engine in global scope
+    
+    Returns:
+    a python list of each column in the database
+    '''
+    with ENGINE.connect() as con: #creating a new connection to the database
+        #Simply checking if an ID column exists in the database or if we need to create one
+        column_names = [] #Every column
+        result = con.execute("PRAGMA table_info('cars')") #returns iterable of tuples
+        for row in result:
+        print(row)
+            column_names.append(row[1])
+    return column_names
+
+def check_if_ID_col_exists(COL_NAME="ID", COLUMN_NAMES=get_column_names()):
+    '''
+    Description:
+    Checks if the current database includes an column with 
+    the name COL_NAME. Returns True if ID column exists, 
+    otherwise it returns False.
+    
+    Parameters:
+    * COLUMN_NAMES ~ a python list of each column in the database
+    * * * * Default: Creates a list with get_column_names method
+    * COL_NAME ~ a python string
+    * * * * Default: "ID"
+    
+    Returns:
+    a bool either True or False. True if the column does exist,
+    False if it does not exist.
+    '''
+    if COL_NAME in COLUMN_NAMES:
+        return True
+    else:
+        return False
+    
+if check_if_ID_col_exists == True:
+    #create ID Column
+    
+    
+#conn = engine.connect()
+#column_names = conn.execute(query).keys()
+#column_names = engine.execute('SELECT * FROM cars').all()
+
+#db = SQLAlchemy(app)
 #db.create_all() #only run once
 
 #Need to check if unique identifier column is in database, if not 
@@ -39,7 +94,7 @@ column_names = db.__table__.columns.keys()
 print(column_names)
 db.session.commit() 
 
-column_names = conn.execute(query).keys()
+#column_names = conn.execute(query).keys()
 
 
 #Combining with SQLAlchemy
@@ -54,3 +109,6 @@ class VideoModel(db.Model):
     car_name = db.Column((db.String(250), primary_key=True)
     name = db.Column(db.String(100), nullable=False)
 '''
+
+if __name__ == "__main__":
+    app.run(debug=True)
